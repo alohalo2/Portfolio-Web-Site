8ee66b5522db3dc13e24c6f654b7bac3c5d81812
DESKTOP-OEDPGKJ\wntjd <wntjd7892@naver.com>
2025-01-23 18:01:23 +0900

    25.01.23

--- components/section-project.js
+++ components/section-project.js
@@ -17,7 +17,7 @@ export default function SectionProject() {
                     </header>
                     <main className={classes.projectContentBox}>
                         <div className={classes.projectContentLeft}>
-                            {/* 이미지 캐러셀 */}
+                            {/* 이미지 캐러셀  */}
                             <ImageCarousel images={project.images} />
                         </div>
                         <div className={classes.projectContentRight}>
@@ -40,7 +40,7 @@ export default function SectionProject() {
                                 <p>
                                     <span>기술스택:</span>
                                     {project.technologies.join(", ")}
-                                </p>   
+                                </p>
                             </div>
                         </div>
                     </main>
